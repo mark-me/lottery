04589e31dccc1b218590fa378a8801ee76cfa8a9
Mark Zwart <mark.zwart@pobox.com>
2019-07-07 16:39:28 +0200

    Added value ranges

--- hello_world.py
+++ hello_world.py
@@ -31,23 +31,40 @@ class Lottery(tk.Tk):
 
 class StartPage(tk.Frame):
 
+    def show_ranges(self):
+
+        self.ticket_numbers = np.array([])
+        self.ticket_ranges = []
+        for entry in self.entry_ticket_ranges:
+            from_value = entry[0].get()
+            to_value = entry[1].get()
+            if(from_value!='' and to_value!=''):
+                self.ticket_ranges.append([int(from_value), int(to_value)])
+
+        for range in self.ticket_ranges:
+            self.ticket_numbers = np.append(self.ticket_numbers, np.arange(range[0], range[1] + 1))
+        print(self.ticket_numbers)
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Start page", font=LARGE_FONT)
-        colors = ['red', 'green', 'yellow', 'orange', 'blue', 'navy']
+        colors = ['Set 1', 'Set 2', 'Set 3', 'Set 4', 'Set 5', 'Set 6']
+        self.entry_ticket_ranges = []
         for c in colors:
             frame = tk.Frame(self)
             label = tk.Label(frame, text=c, relief=tk.RIDGE,  width=25)
             entry_from = tk.Entry(frame, relief=tk.SUNKEN, width=50)
             entry_to = tk.Entry(frame, relief=tk.SUNKEN, width=50)
+            self.entry_ticket_ranges.append([entry_from, entry_to])
             frame.pack(side=tk.TOP)
             label.pack(side=tk.LEFT)
             entry_from.pack(side=tk.LEFT)
             entry_to.pack(side=tk.RIGHT)
-            controller.ticket_ranges.append([entry_from, entry_to])
 
         btn_start = tk.Button(self, text='Start loterij',
-                              command=lambda: controller.show_frame(PageOne))
+                              command=self.show_ranges)
+        #btn_start = tk.Button(self, text='Start loterij',
+        #                      command=lambda: controller.show_frame(PageOne))
         btn_start.pack()
 
 
